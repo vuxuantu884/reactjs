796c8739247421ea22ed04c4c8f5f412eaf8d02d
Vũ xuân Tú <tuvu884884@gmail.com>
2021-09-19 00:13:25 +0700

    logic

--- index.js
+++ index.js
@@ -4,31 +4,69 @@ const $$ = document.querySelectorAll.bind(document);
 let currentIndex = 0;
 const playIcon = $(".fa-play");
 const pauseIcon = $(".fa-pause");
+const nextIcon = $(".fa-step-backward");
+const preIcon = $(".fa-step-forward");
 const audioMusic = Array.from($$(".audio"));
 const listMusic = Array.from($$(".music__item"));
 
-playIcon.onclick = () => {
-    pauseIcon.style.display = "block";
-    playIcon.style.display = "none";
-    pauseIcon.setAttribute("currentIndex", currentIndex);
-    const index = playIcon.getAttribute("currentIndex");
-    if(index) {
-
-    } else {
-        audioMusic[0].play();
+listMusic.forEach((item, index) => {
+    item.onclick = () => {
+        audioMusic[currentIndex].pause();
+        audioMusic[index].play();
+        currentIndex = index;
+        pauseIcon.setAttribute("currentIndex", currentIndex);
+        pauseIcon.style.display = "block";
+        playIcon.style.display = "none";
     }
-}
+});
+
+playIcon.onclick = () => {
+  pauseIcon.style.display = "block";
+  playIcon.style.display = "none";
+  const index = playIcon.getAttribute("currentIndex") || null;
+  if (index) {
+    audioMusic.forEach((item, indexItem) => {
+      if (+index === indexItem) {
+        item.play();
+      }
+    });
+  } else {
+    audioMusic[0].play();
+  }
+  pauseIcon.setAttribute("currentIndex", currentIndex);
+};
 
 pauseIcon.onclick = () => {
-    pauseIcon.style.display = "none";
-    playIcon.style.display = "block";
-    playIcon.setAttribute("currentIndex", currentIndex);
-    const index = pauseIcon.getAttribute("currentIndex");
+  pauseIcon.style.display = "none";
+  playIcon.style.display = "block";
+  const index = pauseIcon.getAttribute("currentIndex");
 
-    if(index) {
+  if (index) {
+    audioMusic.forEach((item, indexItem) => {
+      if (+index === indexItem) {
+        item.pause();
+      }
+    });
+  } else {
+    audioMusic[0].pause();
+  }
+  playIcon.setAttribute("currentIndex", currentIndex) || null;
+};
+preIcon.onclick = () => {
 
+    audioMusic[currentIndex].pause();
+    console.log(audioMusic[currentIndex]);
+    if(currentIndex === audioMusic.length - 1) {
+        currentIndex = 0;
+        pauseIcon.setAttribute("currentIndex", currentIndex);
+        pauseIcon.style.display = "block";
+        playIcon.style.display = "none";
+        audioMusic[0].play();
     } else {
-        audioMusic[0].pause();
+        currentIndex = currentIndex + 1;
+        pauseIcon.setAttribute("currentIndex", currentIndex);
+        pauseIcon.style.display = "block";
+        playIcon.style.display = "none";
+        audioMusic[currentIndex].play();
     }
 }
-
